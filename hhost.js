7e93880cc434d83660cd7e9ba1570b230e1149c6
Hal <hgrforrest@gmail.com>
2021-12-12 15:05:38 -0500

    same but with async function to use awaits

--- hhost.js
+++ hhost.js
@@ -1,15 +1,30 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-	let data = ''
-	req.on('data', chunk => {
-		console.log(`Data chunk available: ${chunk}`)
-		data += chunk
-	})
-	req.on('end', () => {
-		console.log(JSON.parse(data).games[1])
-		res.end()
-	})
+// const server = http.createServer((req, res) => {
+// 	let data = ''
+// 	req.on('data', chunk => {
+// 		console.log(`Data chunk available: ${chunk}`)
+// 		data += chunk
+// 	})
+// 	req.on('end', () => {
+// 		console.log(JSON.parse(data).games[1])
+// 		res.end()
+// 	})
+// })
+
+// same but with awaits 
+
+const server = http.createServer(async (req, res) => {
+	const buffers = []
+
+	for await (const chunk of req) {
+		buffers.push(chunk)
+	}
+
+	const data = Buffer.concat(buffers).toString()
+
+	console.log(JSON.parse(data).games)
+	res.end()
 })
 
 server.listen(8081)
